3e19584ed3977afbc64fcf8b31897baca1dbd334
Mikhail Matz <matz@utexas.edu>
2018-06-08 08:03:37 -0400

    secondary contact, genomic islands

--- IM2_SC_noMig.py
+++ IM2_SC_noMig.py
@@ -57,7 +57,7 @@ def IM2iSC(params, ns):
 
 func=IM2iSC
 upper_bound = [100,100,100, 100, 10,10,0.25]
-lower_bound = [1e-3,1e-3,1e-3,1e-3, 1e-3,1e-3,1e-5,1e-5,1e-5]
+lower_bound = [1e-3,1e-3,1e-3,1e-3,1e-5,1e-5,1e-5]
 params = moments.Misc.perturb_params(params, fold=2, upper_bound=upper_bound,
                               lower_bound=lower_bound)
 
