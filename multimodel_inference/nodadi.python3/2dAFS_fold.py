9355b5fdd1fb1713afe8c384d9edc25870229981
Mikhail Matz <matz@utexas.edu>
2020-08-28 21:17:10 -0500

    two pythons

---
+++ multimodel_inference/nodadi.python3/2dAFS_fold.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use('PDF')
+import moments
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import array
+from moments import Misc,Spectrum,Numerics,Manips,Integration,Demographics1D,Demographics2D
+import sys
+infile=sys.argv[1]
+pop_ids=[sys.argv[2],sys.argv[3]]
+projections=[int(sys.argv[4]),int(sys.argv[5])]
+
+fs = moments.Spectrum.from_file(infile)
+fs=fs.fold()
+data=fs.project(projections)
+ns=data.sample_sizes
+
+'''
+# masking singletons 
+#data.mask[:,0]=True
+#data.mask[0,:]=True
+data.mask[:,1]=True
+data.mask[1,:]=True
+#data.mask[:,projections[1]]=True
+#data.mask[projections[0],:]=True
+data.mask[:,projections[1]-1]=True
+data.mask[projections[0]-1,:]=True
+'''
+np.set_printoptions(precision=3)     
+
+moments.Plotting.plot_single_2d_sfs(data, vmin=0.1)
+plt.savefig('2dAFSf_'+sys.argv[1]+"_"+sys.argv[2]+"_"+sys.argv[3]+"_"+sys.argv[4]+"_"+sys.argv[5]+'.pdf')
+nalleles=data.S()
+print( "N alleles: ",nalleles)
---
+++ multimodel_inference/nodadi.python3/projections_fold.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import moments
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import array
+import sys
+infile=sys.argv[1]
+popid=[sys.argv[2]]
+proj=range(int(sys.argv[3]),int(sys.argv[4]))
+maxS=0
+maxproj=0
+for p in range(len(proj)):
+    data = moments.Spectrum.from_data_dict(dd, pop_ids=popid,
+        projections=[proj[p]],polarized=False)
+    print proj[p],data.S()
+    if maxS<data.S():
+            maxS=data.S()
+            maxproj=proj[p]
+print( "\n------\n",maxproj,maxS,"\n")
