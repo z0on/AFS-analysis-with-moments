2df31f7946930fa4971f147bfa20d9b373bc7ce9
jprippe <jpr6mg@gmail.com>
2020-09-01 17:07:15 -0500

    Fixed indentation errors in folded models

--- multimodel_inference/py2/fold_sc3iml.py
+++ multimodel_inference/py2/fold_sc3iml.py
@@ -19,9 +19,9 @@ infile=sys.argv[1]
 pop_ids=[sys.argv[2],sys.argv[3]]
 projections=[int(sys.argv[4]),int(sys.argv[5])]
 if len(sys.argv)==9:
-  params = np.loadtxt(sys.argv[8], delimiter=" ", unpack=False)
+    params = np.loadtxt(sys.argv[8], delimiter=" ", unpack=False)
 else:
-  params=[1,1,1,1,1,1,1,1,1,1,1,1,1,0.5]
+    params=[1,1,1,1,1,1,1,1,1,1,1,1,1,0.5]
 
 # mutation rate per sequenced portion of genome per generation: for A.millepora, 0.02
 mu=float(sys.argv[6])
